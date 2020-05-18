ae8ec728d1d07bfbe290503c66ab67ad9794c7cd
leonel paredes <leonel.paredes@agilethought.com>
2020-05-18 17:03:03 -0500

    Publish

---
+++ dockertraining_Leonel_Paredes/CustomStoreSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dockertraining_Leonel_Paredes
+{
+    public class CustomStoreSettings
+    {
+        public string StoreName { get; set; }
+    }
+}
---
+++ dockertraining_Leonel_Paredes/ReadMe.txt
@@ -0,0 +1,3 @@
+﻿docker build --tag dockertrainingleonelparedes .
+docker run -dt -p 8484:80 --name Site1 dockertrainingleonelparedes
+docker run -dt -e "CustomStoreSettings:StoreName=Plano" --name Site2 -p 8585:80 dockertrainingleonelparedes
